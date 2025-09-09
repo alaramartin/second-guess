c58a4e1bf17ff2ea385d3212926fe7a0bd149ddc
alaramartin <alara.martin@gmail.com>
2025-09-08 22:21:03 -0700

    very basic gameplay (buttons n stuff) done

--- guess.js
+++ guess.js
@@ -2,7 +2,15 @@ console.log("hi");
 
 let score = 0;
 let numRoundsFinished = 0;
-const numRoundsTotal = 5;
+const numRoundsTotal = 3;
+let isFirstGuess = true;
+
+// set up the game
+function initializeGame() {
+    score = 0;
+    numRoundsFinished = 0;
+    isFirstGuess = true;
+}
 
 // wait until the DOM loads
 document.addEventListener("DOMContentLoaded", () => {
@@ -26,20 +34,33 @@ document.addEventListener("DOMContentLoaded", () => {
     answerInput.addEventListener("submit", async function(event) {
         // prevent the website automatically reloading
         event.preventDefault();
-        numRoundsFinished += 1;
-        // if played all rounds, switch to end game
-        if (numRoundsFinished >= numRoundsTotal) {
-            questionScreen.style.display = "none";
-            endScreen.style.display = "block";
-        } else {
+        if (isFirstGuess) {
             console.log(document.getElementById("answer").value);
-            console.log(`played ${numRoundsFinished} rounds out of ${numRoundsTotal} rounds`);
+            document.getElementById("answer").value = "";
+            document.getElementsByTagName("label")[0].textContent = "Second Guess:";
+            isFirstGuess = false;
+        }
+        else {
+            console.log(document.getElementById("answer").value);
+            document.getElementById("answer").value = "";
+            console.log(`played ${numRoundsFinished + 1} rounds out of ${numRoundsTotal} rounds`);
+            numRoundsFinished += 1;
+            document.getElementsByTagName("label")[0].textContent = "First Guess:";
+            isFirstGuess = true;
+            // if played all rounds, switch to end game
+            if (numRoundsFinished >= numRoundsTotal) {
+                questionScreen.style.display = "none";
+                endScreen.style.display = "block";
+            }
         }
     });
 
-    // set up the game
-    function initializeGame() {
-        score = 0;
-        numRoundsFinished = 0;
-    }
+    const restartButton = document.getElementById("restart");
+    restartButton.addEventListener("click", () => {
+        // go back to start page
+        endScreen.style.display = "none";
+        startScreen.style.display = "block";
+
+        console.log("restarted");
+    });
 });
