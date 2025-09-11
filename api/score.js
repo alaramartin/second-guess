f65a051a6455de826ff71fb281e6beea12a85db9
alaramartin <alara.martin@gmail.com>
2025-09-10 21:57:07 -0700

    started ai integration

---
+++ api/score.js
@@ -0,0 +1,11 @@
+// idea: add api calls to evaluate how funny/silly the first guess was
+
+// note: need a free ai api to call. mistral maybe? gemini <- might be free for educational use
+
+/* prompt
+
+This is a trivia game, where points are based on the humor of the answer given rather than the correctness.
+The humor (funnyness or sillyness) of a response will be rated on a scale of 1-5, where 1 is not funny at all and 5 is the funniest.
+
+... give examples...
+*/
